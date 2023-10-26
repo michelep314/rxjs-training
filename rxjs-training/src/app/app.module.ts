06b165283582951d69c898c966b5a15428406feb
Oversize07 <0v3rfl0w.07@gmail.com>
2023-10-26 13:01:36 +0200

    First commit

---
+++ rxjs-training/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FibonacciComponent } from './fibonacci/fibonacci.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FibonacciComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
